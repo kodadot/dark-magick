e62e06a1256878573046bb304294fe6fd8f060bd
Viki Val <viktorko99@gmail.com>
2024-07-04 12:26:50 +0200

    :construction: WIP migration

--- src/processor.ts
+++ src/processor.ts
@@ -1,17 +1,47 @@
 import { lookupArchive } from '@subsquid/archive-registry'
-import { SubstrateProcessor } from '@subsquid/substrate-processor'
-import { FullTypeormDatabase as Database } from '@subsquid/typeorm-store'
+import {
+    SubstrateBatchProcessor as SubstrateProcessor
+  } from '@subsquid/substrate-processor'
+  import { TypeormDatabase as Database } from '@subsquid/typeorm-store'
 import * as mappings from './mappings'
 
-const processor = new SubstrateProcessor(new Database())
+export const fieldSelection = {
+    block: {
+      timestamp: true
+    },
+    extrinsic: {
+      signature: true,
+    },
+    call: {
+        name: true,
+        args: true,
+        origin: true
+    },
+    event: {
+        name: true,
+        args: true,
+    }
+  } as const
+  
+  export type SelectedFields = typeof fieldSelection
+
+const database = new Database({ supportHotBlocks: false })
+const processor = new SubstrateProcessor<SelectedFields>()
+
+// const processor = new SubstrateProcessor(new Database())
 
 processor.setTypesBundle('kusama');
 processor.setBlockRange({from:5756453});
-processor.setDataSource({
-    archive: lookupArchive("kusama", { release: "FireSquid" }),
-    chain: 'wss://kusama-rpc.polkadot.io'
-});
+processor.setGateway('https://v2.archive.subsquid.io/network/kusama')
+processor.setRpcEndpoint({
+    url: 'wss://kusama-rpc.polkadot.io',
+    rateLimit: 10
+  })
 
-processor.addCallHandler('System.remark', mappings.handleRemark);
 
-processor.run();
+
+// processor.addCallHandler('System.remark', mappings.handleRemark);
+processor.addCall({ name: ['System.remark'], extrinsic: true })
+
+// processor.run();
+// processor.run(database, mainFrame)
